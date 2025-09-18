1931158b73e9beba7b9760f7029411d7abb6227d
Nazanin <dev.nazaninazar@gmail.com>
2025-09-18 15:16:44 +0330

    feat: display list of products from API

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,6 @@ import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-   modules: ["@nuxtjs/tailwindcss", "nuxt-icon"]
+   modules: ["@nuxtjs/tailwindcss"]
 })
 
