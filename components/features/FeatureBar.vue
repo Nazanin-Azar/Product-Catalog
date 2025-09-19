ba9fd9d64c22a069c74696fa233b958027bb6627
Nazanin <dev.nazaninazar@gmail.com>
2025-09-19 17:59:05 +0330

    feat: implement feature bar section

---
+++ components/features/FeatureBar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="bg-[#1c1917] py-12 md:py-16 p-0">
+    <div class="container mx-auto">
+      <div
+        class="flex flex-col md:flex-row justify-around items-center gap-y-8 md:gap-0">
+        <template v-for="(feature, index) in features" :key="index">
+          <div class="flex items-center gap-4 w-full lg:w-auto px-4">
+            <div>
+              <Icon :name="feature.icon" class="text-4xl text-[#8F5D45]" />
+            </div>
+            <div class="text-left">
+              <h3 class="font-semibold text-white">{{ feature.title }}</h3>
+              <p class="text-sm text-gray-300">{{ feature.description }}</p>
+            </div>
+          </div>
+
+          <div
+            v-if="index < features.length - 1"
+            class="hidden md:block h-20 w-px bg-gray-600"></div>
+        </template>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const features = ref([
+  {
+    icon: "uil:truck",
+    title: "Free & Fast Shipping",
+    description: "On all orders over $100",
+  },
+  {
+    icon: "uil:exchange-alt",
+    title: "Product Exchange Guarantee",
+    description: "30-day return policy",
+  },
+  {
+    icon: "uil:headphones-alt",
+    title: "24/7 Customer Support",
+    description: "Contact us anytime",
+  },
+  {
+    icon: "uil:shield-check",
+    title: "Authenticity Guaranteed",
+    description: "100% genuine products",
+  },
+]);
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,55 @@
 <template>
-  <div> 
+  <div>
     <div class="w-screen">
-            <img src="../assets/img/banner.png" alt="banner" class="w-full">
-        </div>
-  <div class=" mx-auto p-4">
-   
-    <div v-if="pending" class="text-center">Loading...</div>
-
-    <div v-if="error" class="text-center text-red-500">
-      Failed to fetch products. Please try again.
+      <img src="../assets/img/banner.png" alt="banner" class="w-full" />
     </div>
+    <div class="mx-auto p-4">
+      <div v-if="pending" class="text-center">Loading...</div>
 
-    <div
-    v-else
-      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-      <ProductCard
-        v-for="product in products"
-        :key="product.id"
-        :product="product" />
+      <div v-if="error" class="text-center text-red-500">
+        Failed to fetch products. Please try again.
+      </div>
+
+      <div
+        v-else
+        class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <ProductCard
+          v-for="product in displayedProducts"
+          :key="product.id"
+          :product="product" />
+      </div>
+      <div
+        v-if="displayedProductsCount < products.length"
+        class="flex justify-end mt-10 mb-10">
+        <button
+          @click="showMoreProducts"
+          class="text-[#8F5D45] font-medium py-3 px-4 rounded-3xl flex items-center gap-2 hover:shadow-lg transition-colors border border-[#8F5D45]">
+          <span>More </span>
+          <Icon
+            name="uil:angle-right-b"
+            class="transition-transform duration-300 ease-in-out group-hover:translate-x-2" />
+        </button>
+      </div>
     </div>
-
-  </div>
+    <FeaturesFeatureBar />
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
-import ProductCard from '~/components/products/ProductCard.vue';
-import { useProducts } from '~/composables/useProducts';
+import { computed } from "vue";
+import ProductCard from "~/components/products/ProductCard.vue";
+import { useProducts } from "~/composables/useProducts";
 
 const { getProducts } = useProducts();
 const { data, pending, error } = getProducts();
 const products = computed(() => data.value?.products || []);
-console.log('Parent page: Computed products object:', products);
+
+const displayedProductsCount = ref(8);
+const displayedProducts = computed(() => {
+  return products.value.slice(0, displayedProductsCount.value);
+});
+
+function showMoreProducts() {
+  displayedProductsCount.value = products.value.length;
+}
 </script>
