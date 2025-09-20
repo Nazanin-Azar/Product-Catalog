a33d9691af6631b656c94339f0edfd9c64ef4bc2
Nazanin <dev.nazaninazar@gmail.com>
2025-09-20 19:09:36 +0330

    fix: fix some SSR errors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", 'nuxt-icon'],
+   build: {
+    transpile: ["vue-toastification"],  
+  },
    css: [
     "vue-toastification/dist/index.css"
   ],
